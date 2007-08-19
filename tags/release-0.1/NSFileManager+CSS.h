4e0591555980054278bb3c5431cb2ae6ec1f454f
nseriot <nseriot@e059ab23-0438-0410-81eb-03c85d175112>
2007-08-19 02:44:21 +0000

    tag on release 0.1

---
+++ tags/release-0.1/NSFileManager+CSS.h
@@ -0,0 +1,16 @@
+//
+//  NSFileManager+CSS.h
+//  CocoaSlideShow
+//
+//  Created by Nicolas Seriot on 17.08.07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSFileManager (CSS)
+
+- (NSArray *)directoryContentFullPaths:(NSString*)dirPath recursive:(BOOL)isRecursive;
+
+@end
