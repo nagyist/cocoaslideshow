bb2caa99d8c37e310572a744172b2d81e22b5258
nseriot <nseriot@e059ab23-0438-0410-81eb-03c85d175112>
2008-06-08 15:08:16 +0000

    tag on release 0.4.5

---
+++ tags/release-0.4.5/CSSImageContainer.m
@@ -0,0 +1,104 @@
+//
+//  CSSImageContainer.m
+//  CocoaSlideShow
+//
+//  Created by Nicolas Seriot on 25.08.07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "CSSImageContainer.h"
+#import "NSString+CSS.h"
+
+@implementation CSSImageContainer
+
+/*
+- (id)init {
+	self = [super init];
+	NSLog(@"init %@", self);
+	return self;
+}
+*/
+
++ (void)initialize {
+    [self setKeys:[NSArray arrayWithObjects:@"isFlagged", nil] triggerChangeNotificationsForDependentKey:@"flagIcon"];
+}
+
+- (CSSBitmapImageRep *)bitmap {	
+	BOOL importDone = [[[NSApp delegate] valueForKeyPath:@"imagesController.importDone"] boolValue];
+	BOOL isSaving = [[[NSApp delegate] valueForKey:@"isSaving"] boolValue];
+	BOOL multipleImagesSelected = [[[NSApp delegate] valueForKeyPath:@"imagesController.multipleImagesSelected"] boolValue];
+
+	if(!importDone || (multipleImagesSelected && !isSaving) ) {
+		return nil;
+	}
+
+	if(bitmap != nil) {
+		return bitmap;
+	}
+
+	NSData *data = [NSData dataWithContentsOfFile:path];
+
+	bitmap = [[CSSBitmapImageRep alloc] initWithData:data];
+	[bitmap setPath:path];
+
+	return bitmap;
+}
+
+- (void)dealloc {
+	[path release];
+	[bitmap release];
+	[super dealloc];
+}
+
+- (void)forgetBitmap {
+	[bitmap release];
+	bitmap = nil;
+}
+
+- (void)flag {
+	[self setValue:[NSNumber numberWithBool:YES] forKey:@"isFlagged"];
+}
+
+- (void)unflag {
+	[self setValue:[NSNumber numberWithBool:NO] forKey:@"isFlagged"];
+}
+
+- (void)toggleFlag {
+	[self setValue:[NSNumber numberWithBool:!isFlagged] forKey:@"isFlagged"];
+}
+
+- (void)removeFlag {
+	[self setValue:[NSNumber numberWithBool:NO] forKey:@"isFlagged"];	
+}
+
+- (BOOL)isFlagged {
+	return isFlagged;
+}
+
+- (NSImage *)flagIcon {
+	return isFlagged ? [NSImage imageNamed:@"Flagged.png"] : nil;
+}
+
+- (void)copyToDirectory:(NSString *)destDirectory {
+	NSString *destPath = [destDirectory stringByAppendingPathComponent:[path lastPathComponent]];
+	NSFileManager *fm = [NSFileManager defaultManager];
+
+	if ([fm fileExistsAtPath:path]) {
+		[fm copyPath:path toPath:destPath handler:nil];
+	}
+}
+
+- (void)moveToTrash {
+	NSString *trashPath = [[@"~/.Trash/" stringByExpandingTildeInPath] stringByAppendingPathComponent:[path lastPathComponent]];
+	[[NSFileManager defaultManager] movePath:path toPath:trashPath handler:nil];
+}
+
+- (void)revealInFinder {
+	[[NSWorkspace sharedWorkspace] selectFile:path inFileViewerRootedAtPath:@""];
+}
+
+- (NSString *)path {
+	return path;
+}
+
+@end
---
+++ tags/release-0.4.5/ImagesController.m
@@ -0,0 +1,177 @@
+#import "ImagesController.h"
+#import "CSSImageContainer.h"
+#import "NSFileManager+CSS.h"
+
+@implementation ImagesController
+
+- (void)awakeFromNib {
+	inMemoryBitmapsContainers = [[NSMutableArray alloc] initWithCapacity:IN_MEMORY_BITMAPS];
+}
+
+- (void)dealloc {
+	[inMemoryBitmapsContainers release];
+	[super dealloc];
+}
+
+- (NSUndoManager *)undoManager {
+	return [cocoaSlideShow undoManager];
+}
+
+- (NSIndexSet *)flaggedIndexes {
+	NSMutableIndexSet *flaggedIndexes = [[NSMutableIndexSet alloc] init];
+	
+	NSEnumerator *e = [[self arrangedObjects] objectEnumerator];
+	CSSImageContainer *container;
+	while(( container = [e nextObject] )) {
+		if([container isFlagged]) {
+			[flaggedIndexes addIndex:[[self arrangedObjects] indexOfObject:container]];
+		}
+	}
+	[self setSelectionIndexes:flaggedIndexes];
+	return [flaggedIndexes autorelease];
+}
+
+- (void)flagIndexes:(NSIndexSet *)indexSet {
+	[[[self arrangedObjects] objectsAtIndexes:indexSet] makeObjectsPerformSelector:@selector(flag)]; 
+}
+
+- (IBAction)flag:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] unflag:self];
+	[[self selectedObjects] makeObjectsPerformSelector:@selector(flag)];
+}
+
+- (IBAction)unflag:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] flag:self];
+	[[self selectedObjects] makeObjectsPerformSelector:@selector(unflag)];
+}
+
+- (IBAction)toggleFlags:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] toggleFlags:self];
+	[[self selectedObjects] makeObjectsPerformSelector:@selector(toggleFlag)];
+}
+
+- (IBAction)selectFlags:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] setSelectionIndexes:[self selectionIndexes]];
+	[self setSelectionIndexes:[self flaggedIndexes]];
+}
+
+- (IBAction)removeAllFlags:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] flagIndexes:[self flaggedIndexes]];
+	[[self arrangedObjects] makeObjectsPerformSelector:@selector(removeFlag)];
+}
+
+- (IBAction)remove:(id)sender {
+	[[[self undoManager] prepareWithInvocationTarget:self] performSelector:@selector(addFiles:) withObject:[self valueForKeyPath:@"selectedObjects.path"]];
+	[super remove:sender];
+}
+
+- (void)selectPreviousImage {
+	if([self canSelectPrevious]) {
+		[[[self undoManager] prepareWithInvocationTarget:self] selectNext:self];
+		[self selectPrevious:self];
+	}
+}
+
+- (void)selectNextImage {
+	if([self canSelectNext]) {
+		[[[self undoManager] prepareWithInvocationTarget:self] selectPrevious:self];
+		[self selectNext:self];
+	}
+}
+
+- (BOOL)multipleImagesSelected {
+	return [[self selectedObjects] count] > 1;
+}
+
+- (BOOL)containsPath:(NSString *)path {
+	NSArray *paths = [self valueForKeyPath:@"arrangedObjects.path"];
+	return [paths containsObject:path];
+}
+
+- (void)addFiles:(NSArray *)filePaths {
+
+	importDone = NO;
+	
+	CSSImageContainer *firstInsertedObject = nil;
+
+	NSEnumerator *e = [filePaths objectEnumerator];
+	NSString *path;
+   // http://developer.apple.com/documentation/Cocoa/Conceptual/CocoaDrawingGuide/Images/chapter_7_section_3.html
+	NSArray *allowedExtensions = [NSArray arrayWithObjects:@"jpg", @"jpeg", @"jpe", @"tif", @"tiff", @"gif", @"png", @"pct", @"pict", @"pic",
+	                                                       //@"pdf", @"eps", @"epi", @"epsf", @"epsi", @"ps",
+														   @"ico", @"icns",  @"bmp", @"bmpf",
+														   @"dng", @"cr2", @"crw", @"fpx", @"fpix", @"raf", @"dcr", @"ptng", @"pnt", @"mac", @"mrw", @"nef",
+														   @"orf", @"exr", @"psd", @"qti", @"qtif", @"hdr", @"sgi", @"srf", @"targa", @"tga", @"cur", @"xbm", nil];
+
+	NSMutableArray *containersToAdd = [[NSMutableArray alloc] init];
+	
+	NSArray *dirContent;
+	while(( path = [e nextObject] )) {
+		NSString *ext = [path pathExtension];
+		
+		dirContent = [[NSFileManager defaultManager] directoryContentFullPaths:path recursive:YES];
+		if(dirContent) {
+			[self addFiles:dirContent];
+		}
+		
+		if([path hasPrefix:@"."] || ![allowedExtensions containsObject:[ext lowercaseString]]) {
+			continue;
+		}
+		
+		CSSImageContainer *container = [[CSSImageContainer alloc] init];
+		[container setValue:path forKey:@"path"];
+		
+		if(firstInsertedObject == nil) {
+			firstInsertedObject = container;
+		}
+		[containersToAdd addObject:[container autorelease]]; // TODO: don't add one by one but alltogether
+	}
+	
+	[self addObjects:containersToAdd];
+	[containersToAdd release];
+	
+	importDone = YES;
+	/*
+	if(firstInsertedObject != nil) {
+		[self setSelectedObjects:[NSArray arrayWithObject:firstInsertedObject]];
+	}
+	*/
+}
+
+- (void)addDirFiles:(NSString *)dir {
+	[self addFiles:[NSArray arrayWithObject:dir]];
+	[[[self undoManager] prepareWithInvocationTarget:self] remove:self];
+}
+
+- (IBAction)moveToTrash:(id)sender {
+	[[self selectedObjects] makeObjectsPerformSelector:@selector(moveToTrash)];
+	[self removeObjectsAtArrangedObjectIndexes:[self selectionIndexes]];
+}
+
+- (void) retainOnlyAFewImagesAndReleaseTheRest {
+	if([[self selectedObjects] count] != 1) {
+		return;
+	}
+	
+	CSSImageContainer *c = [[self selectedObjects] lastObject];
+	
+	if(![inMemoryBitmapsContainers containsObject:c]) {
+		if([inMemoryBitmapsContainers count] == IN_MEMORY_BITMAPS) {
+			CSSImageContainer *oldContainer = [inMemoryBitmapsContainers objectAtIndex:0];
+			[oldContainer forgetBitmap];
+			[inMemoryBitmapsContainers removeObject:oldContainer];
+		}
+		[inMemoryBitmapsContainers addObject:c];
+	}
+}
+/*
+- (NSArray *)flagged {
+	NSPredicate *p = [NSPredicate predicateWithFormat:@"flagged == YES"];
+	NSMutableArray *ma = [[self arrangedObjects] mutableCopy];
+	[ma filterUsingPredicate:p];
+	return [ma autorelease];
+}
+*/
+@end
+
+
