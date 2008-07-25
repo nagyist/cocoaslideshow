8c81d9c1bcdcc7019b283cdd503024fbd474035e
nseriot <nseriot@e059ab23-0438-0410-81eb-03c85d175112>
2008-07-25 17:58:12 +0000

    new toolbar images by Jan Peratt, improved toolbar items validation

--- trunk/CocoaSlideShow+Toolbar.m
+++ trunk/CocoaSlideShow+Toolbar.m
@@ -19,64 +19,64 @@
         [item setLabel:NSLocalizedString(@"Set Directory…", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Set Directory…", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Set Directory…", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"folder.pdf"]];
+        [item setImage:[NSImage imageNamed:@"folder.png"]];
         [item setTarget:self];
         [item setAction:@selector(setDirectory:)];
 	} else if ([itemIdentifier isEqualToString:@"addFiles"]) {
         [item setLabel:NSLocalizedString(@"Add Files…", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Add Files…", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Add Files…", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"add.pdf"]];
+        [item setImage:[NSImage imageNamed:@"add.png"]];
         [item setTarget:self];
         [item setAction:@selector(addDirectory:)];
 	} else if ([itemIdentifier isEqualToString:@"flag"]) {
         [item setLabel:NSLocalizedString(@"Flag", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Flag", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Flag", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"flag.pdf"]];
-        [item setTarget:imagesController];
+        [item setImage:[NSImage imageNamed:@"flag.png"]];
+        [item setTarget:self];
         [item setAction:@selector(toggleFlags:)];
 	} else if ([itemIdentifier isEqualToString:@"fullScreen"]) {
         [item setLabel:NSLocalizedString(@"Full Screen", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Full Screen", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Full Screen", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"fullscreen.pdf"]];
+        [item setImage:[NSImage imageNamed:@"fullscreen.png"]];
         [item setTarget:self];
 		[item setAction:@selector(fullScreenMode:)];
 	} else if ([itemIdentifier isEqualToString:@"slideShow"]) {
         [item setLabel:NSLocalizedString(@"Slideshow", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Slideshow", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Slideshow", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"slideshow.pdf"]];
+        [item setImage:[NSImage imageNamed:@"slideshow.png"]];
         [item setTarget:self];
 		[item setAction:@selector(startSlideShow:)];
 	} else if ([itemIdentifier isEqualToString:@"rotateLeft"]) {
         [item setLabel:NSLocalizedString(@"Rotate Left", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Rotate Left", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Rotate Left", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"left.pdf"]];
+        [item setImage:[NSImage imageNamed:@"left.png"]];
         [item setTarget:self];
 		[item setAction:@selector(rotateLeft:)];
 	} else if ([itemIdentifier isEqualToString:@"rotateRight"]) {
         [item setLabel:NSLocalizedString(@"Rotate Right", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Rotate Right", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Rotate Right", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"right.pdf"]];
+        [item setImage:[NSImage imageNamed:@"right.png"]];
         [item setTarget:self];
 		[item setAction:@selector(rotateRight:)];
 	} else if ([itemIdentifier isEqualToString:@"remove"]) {
         [item setLabel:NSLocalizedString(@"Remove", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Remove", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Remove", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"remove.pdf"]];
-        [item setTarget:imagesController];
+        [item setImage:[NSImage imageNamed:@"remove.png"]];
+        [item setTarget:self];
 		[item setAction:@selector(remove:)];
 	} else if ([itemIdentifier isEqualToString:@"trash"]) {
         [item setLabel:NSLocalizedString(@"Trash", @"Toolbar item")];
         [item setPaletteLabel:NSLocalizedString(@"Trash", @"Toolbar customize")];
         [item setToolTip:NSLocalizedString(@"Trash", @"Toolbar tooltip")];
-        [item setImage:[NSImage imageNamed:@"trash.pdf"]];
-        [item setTarget:imagesController];
+        [item setImage:[NSImage imageNamed:@"trash.png"]];
+        [item setTarget:self];
 		[item setAction:@selector(moveToTrash:)];
     }
 	
@@ -97,7 +97,7 @@
 	NSArray *moreItems = [NSArray array];
 	return [[[self toolbarDefaultItemIdentifiers:nil] arrayByAddingObjectsFromArray:standardItems] arrayByAddingObjectsFromArray:moreItems];
 }
-
+/*
 - (void)toolbaritemclicked:(NSToolbarItem*)item {
     //NSLog(@"-- toolbaritemclicked %@", [item label]);
     
@@ -108,17 +108,28 @@
         //[self openQuickLook:nil];
     }    
 }
-
+*/
 - (BOOL)validateToolbarItem:(NSToolbarItem *)theItem {
     
 	NSString *identifier = [theItem itemIdentifier];
-    if ([identifier isEqualToString:@"informations"]) {
-		return YES;//[[activeTracksController arrangedObjects] count] > 0;
-    } else if ([identifier isEqualToString:@"quickLook"]) {
-		return NO;//quickLookAvailable && [[selectedResultsController selectedObjects] count] > 0;
-	}
+	NSArray *atLeastOneImageSelectedToBeActive = [NSArray arrayWithObjects:@"flag", @"rotateLeft", @"rotateRight", @"remove", @"trash", nil];
+    if ([atLeastOneImageSelectedToBeActive containsObject:identifier]) {
+		return [[imagesController selectedObjects] count] > 0;
+    }
     
     return YES;
+}
+
+- (void)toggleFlags:(id)sender {
+	[imagesController toggleFlags:sender];
+}
+
+- (void)remove:(id)sender {
+	[imagesController remove:sender];
+}
+
+- (void)moveToTrash:(id)sender {
+	[imagesController moveToTrash:sender];
 }
 
 @end
