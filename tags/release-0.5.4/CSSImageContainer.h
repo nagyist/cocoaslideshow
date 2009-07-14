cb905d78904f31a15958de03451f85a41a5ee906
nseriot <nseriot@e059ab23-0438-0410-81eb-03c85d175112>
2009-07-14 07:57:18 +0000

    tag on release 0.5.4

---
+++ tags/release-0.5.4/CSSImageContainer.h
@@ -0,0 +1,53 @@
+//
+//  CSSImageContainer.h
+//  CocoaSlideShow
+//
+//  Created by Nicolas Seriot on 25.08.07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CSSImageContainer : NSObject {
+	NSString *path;
+	CGImageSourceRef source;
+	NSMutableDictionary *metadata;
+
+	BOOL sourceRead;
+	BOOL isFlagged;
+	BOOL isLoadingCache;
+	BOOL isJpeg;
+}
+
+- (NSString *)exifDateTime;
+
+- (NSString *)prettyLatitude;
+- (NSString *)prettyLongitude;
+
+- (BOOL)loadSource;
+
+- (NSImage *)image;
+
+- (NSString *)path;
+- (NSImage *)image;
+
+- (NSDictionary *)exif;
+- (NSDictionary *)gps;
+
++ (CSSImageContainer *)containerWithPath:(NSString *)aPath;
+
+- (NSString *)path;
+
+- (NSURL *)googleMapsURL;
+
+- (NSString *)fileName;
+- (void)setFileName:(NSString *)s;
+
+- (void)flag;
+- (void)unflag;
+- (void)toggleFlag;
+- (void)removeFlag;
+- (BOOL)isFlagged;
+- (NSImage *)flagIcon;
+
+@end
---
+++ tags/release-0.5.4/MyWindow.m
@@ -0,0 +1,35 @@
+#import "MyWindow.h"
+#import "CocoaSlideShow.h"
+
+@implementation MyWindow
+
+- (BOOL)preservesContentDuringLiveResize {
+	return NO;
+}
+
+- (void)keyDown:(NSEvent *)theEvent {
+	CocoaSlideShow *css = [self delegate];
+	
+	//NSLog(@"-- %d", [theEvent keyCode]);
+	
+	switch([theEvent keyCode]) {
+		case 49: // space
+			[css toggleSlideShow:self];
+			break;
+		case 53: // esc
+			[css exitFullScreen:self];
+			break;
+		case 123: // left
+			[css invalidateTimer];
+			[imagesController selectPreviousImage];
+			break;
+		case 124: // right
+			[css invalidateTimer];
+			[imagesController selectNextImage];
+			break;
+		default:
+			[super keyDown:theEvent];
+	}
+}
+
+@end
